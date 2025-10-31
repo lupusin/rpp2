917e5cf9f6f30eadd5d882d742bd845567a86a14
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-10-31 11:43:08 +0700

    ошибка линтера

--- test_discriminant.py
+++ test_discriminant.py
@@ -3,16 +3,15 @@ from discriminant import calculate_discriminant
 
 
 class TestDiscriminant(unittest.TestCase):
-    # Позитивные тесты
+    
     def test_positive_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, -3, 2), 1)  # D > 0
-
+        self.assertEqual(calculate_discriminant(1,-3,2),1)  
+    
     def test_zero_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, -2, 1), 990)  # D = 0
-
-    # Негативные тесты
+        self.assertEqual(calculate_discriminant(1, -2, 1), 0)
+    
     def test_negative_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, 2, 5), -16)  # D < 0
+        self.assertEqual(calculate_discriminant(1,2,5),-16)  
 
 
 if __name__ == "__main__":
