6434530aadbf5eddfcf2c65afd14ceb3763dfb20
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-10-31 11:39:45 +0700

    дискриминант и тест

--- test_discriminant.py
+++ test_discriminant.py
@@ -5,14 +5,14 @@ from discriminant import calculate_discriminant
 class TestDiscriminant(unittest.TestCase):
     # Позитивные тесты
     def test_positive_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, -3, 2), 1)
+        self.assertEqual(calculate_discriminant(1, -3, 2), 1)  # D > 0
 
     def test_zero_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, -2, 1), 990)  
+        self.assertEqual(calculate_discriminant(1, -2, 1), 0)  # D = 0
 
     # Негативные тесты
     def test_negative_discriminant(self):
-        self.assertEqual(calculate_discriminant(1, 2, 5), -16)  
+        self.assertEqual(calculate_discriminant(1, 2, 5), -16)  # D < 0
 
 
 if __name__ == "__main__":
