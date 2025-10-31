9eb6568a5620b1eaf3acb58984067f64a56b5438
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-10-31 11:22:03 +0700

    дискриминант и тест

--- lab3/discriminant.py
+++ lab3/discriminant.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
         discriminant = calculate_discriminant(a, b, c)
         print(f"Дискриминант: {discriminant}")
     except ValueError:
-        print("Ошибка: введите числовые значения коэффициентов.")
+        print("Ошибка: введите числовые значения коэффициентов.")#
